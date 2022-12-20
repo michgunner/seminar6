8d5ea28e7df77174440ab34e0f1339d6e9ed9daf
michgunner <kron.thunder@outlook.com>
2022-12-20 11:00:45 +0300

    Uncomment

--- Program.cs
+++ Program.cs
@@ -32,9 +32,9 @@ void Show2dArray(int[,] array)
 }
 
 
-//Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экран.
+//Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экран
 
-/*
+
 int[,] CreateRandomSumArray()
 {
    Console.WriteLine("Please, enter number of rows: ");
@@ -55,16 +55,16 @@ int[,] CreateRandomSumArray()
 int[,] myArray = CreateRandomSumArray();
 Show2dArray(myArray);
 
-*/
+
 
 
 //Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
 
-/* void EvenIndex(int[,] array)
+ void EvenIndex(int[,] array)
 {
 
-   for(int i = 0; i < array.GetLength(0); i++)
-      for(int j = 0; j < array.GetLength(1); j++)
+   for(int i = 0; i < array.GetLength(0); i+=2)
+      for(int j = 0; j < array.GetLength(1); j+=2)
          if(i%2==0 && j%2==0)
             array[i,j] = array[i,j] * array[i,j];
 
@@ -72,8 +72,7 @@ Show2dArray(myArray);
 int[,] myArray = CreateRandom2dArray();
 Show2dArray(myArray);
 EvenIndex(myArray);
-Show2dArray(myArray); */
-
+Show2dArray(myArray); 
 
 //Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
 
